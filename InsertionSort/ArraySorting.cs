1bcc91feeed733d3a1a18f7c03a08aaa96058cb2
Wixiam <k.aziza.i@mail.ru>
2019-12-23 16:21:02 +0600

    Add project files.

---
+++ InsertionSort/ArraySorting.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace InsertionSort
+{
+    class ArraySorting
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void FillArray(int[] array)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 10);
+            }
+        }
+
+        public void ArrayInsertionSorting(int[] array)
+        {
+            try
+            {
+                int temp;
+                var beforeSort = string.Join(" ", array);
+
+                for (int i = 1; i < array.Length; i++)
+                {
+                    for (int j = i; j > 0 && array[j - 1] > array[j]; j--)
+                    {
+                        temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+
+                var afterSort = string.Join(" ", array);
+                var totalBeforeSort = string.Concat("Result before sort:" + " " + beforeSort);
+                var totalAfterSort = string.Concat("Result after sort:" + " " + afterSort);
+                var str = string.Concat(totalBeforeSort, totalAfterSort);
+
+                logger.Info(str);
+            }
+            catch (Exception ex)
+            {
+                logger.Warn(ex.Message);
+            }
+        }
+
+     
+    }
+}
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array = new int[10];
+
+            ArraySorting arraySorting = new ArraySorting();
+            arraySorting.FillArray(array);
+            Console.WriteLine("Before sort:");
+
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+            arraySorting.ArrayInsertionSorting(array);
+            Console.WriteLine("\nAfter sort:");
+
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
